18209e94c4af0392dbbea42aea14a0b7af293659
Beksultan <beka47674@gmail.com>
2024-11-27 07:52:13 +0500

    ui jumystary salyndy

---
+++ Hw-13week js1/script.js
@@ -0,0 +1,10 @@
+let football = ["Роналду", "Зидан", "Неймар", "Бензема"];
+
+football[1] = "Месси";
+football.push("Винисус", "Холанд");
+if (football.length > 5) {
+    console.log("көп");
+} else {
+    console.log("аз");
+}
+console.log(football);
