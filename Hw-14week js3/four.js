036c2d0f239f7f91c96adbb63d82760a2e9f0554
Beksultan <beka47674@gmail.com>
2024-12-04 23:53:31 +0500

    updated files

---
+++ Hw-14week js3/four.js
@@ -0,0 +1,28 @@
+// //1 TAPSYRMA
+// let okushy = {
+//     name: "Asan",
+//     age: 15,
+//     grades: {
+//         math: 85,
+//         english: 90,
+//         science: 80
+//     },
+//     adress: {
+//         city: "Almaty",
+//         street: "Abaya",
+//         gorod: "Kazakhstan"
+//     }
+// };
+// const { name, grades: { math }, adress: { city }, adress: { gorod: eli = "Қазақстан" } } = okushy;
+// console.log(`Esimi: ${name}`);
+// console.log(`Matematika bagasy: ${math}`);
+// console.log(`Qalasy: ${city}`);
+// console.log(`Memleketi: ${eli}`);
+
+
+
+
+
+//2 TAPSYRMA
+// const people = ["Директор", "Амина", "Әділ", "Әлина", "Берік", "Бақыт", "Баян", "Мұғалім"];
+// console.log(people);
